b71e86fb699e4bc772a353a26c8e9566d85b4142
wcrasta <warrencrasta@gmail.com>
2021-05-23 17:51:29 -0500

    Redirecting HTTP to HTTPS

---
+++ src/main/java/com/warrencrasta/fantasy/yahoo/config/ssl/SSLConfig.java
@@ -0,0 +1,41 @@
+package com.warrencrasta.fantasy.yahoo.config.ssl;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SSLConfig {
+
+  /* https://mkyong.com/spring-boot/spring-boot-redirect-port-8080-to-8443/ */
+  @Bean
+  public ServletWebServerFactory servletContainer() {
+    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+      @Override
+      protected void postProcessContext(Context context) {
+        SecurityConstraint securityConstraint = new SecurityConstraint();
+        securityConstraint.setUserConstraint("CONFIDENTIAL");
+        SecurityCollection collection = new SecurityCollection();
+        collection.addPattern("/*");
+        securityConstraint.addCollection(collection);
+        context.addConstraint(securityConstraint);
+      }
+    };
+    tomcat.addAdditionalTomcatConnectors(redirectConnector());
+    return tomcat;
+  }
+
+  private Connector redirectConnector() {
+    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    connector.setScheme("http");
+    connector.setPort(8080);
+    connector.setSecure(false);
+    connector.setRedirectPort(8443);
+    return connector;
+  }
+}
